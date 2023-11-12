937e43b96d9917cd7e9caa54212e34146afa493a
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-11-12 14:07:17 +0100

    Action / Outlets

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    @IBOutlet weak var questionCountLabel: UILabel!
+    @IBOutlet weak var progressBarView: UIView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: Setup Button
+    func SetupButton(){
+        // Tag vergeben
+        
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func answerButton_Tapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 
   
