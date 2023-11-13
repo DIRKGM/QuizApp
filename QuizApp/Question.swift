3574199ac3e2d800f09cdff8bba134c4cb9c8995
Dirk Meyer <65828797+DIRKGM@users.noreply.github.com>
2023-11-13 14:00:49 +0100

    Question Class

---
+++ QuizApp/Question.swift
@@ -0,0 +1,31 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Dirk Meyer on 13.11.23.
+//
+
+import Foundation
+
+class Question {
+    
+    // Eigenschaften
+    
+    var questionText : String = ""
+    var answer1 : String = ""
+    var answer2 : String = ""
+    var answer3 : String = ""
+    var correvtAnswerTag : Int = 0
+    
+    // Init
+    init(text : String) {
+        questionText = text
+    }
+    
+    // Methoden / Funktionen
+    
+    
+    
+}
+
+
